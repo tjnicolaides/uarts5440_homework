b5d0ff7c7360e31c4cc8983a1772cffd0884656b
TJ Nicolaides <tjnicolaides@gmail.com>
2013-07-05 21:48:38 -0400

    example of completed homework from week #3

--- main.js
+++ main.js
@@ -1,41 +1,51 @@
-var tasks = [{
-        'name' : 'Defragment hard drive',
+
+$(document).ready(function(){
+
+    var tasks = [{
+        'name' : 'Do the laundry',
         'complete' : true
     }, {
-        'name' : 'Do the laundry'
-    }, {
+        'name': 'Take out the trash',
         'complete' : false
     }, {
         'name' : 'Feed the dog',
         'complete' : false
     }];
-
-document.write('<h2>My Tasks</h2>');
-document.write('<ol>');
-
-for(var i= 0; i < tasks.length; i++) {
-    try {
-        printTask(tasks[i]);
-    } catch (msg) {
-        console.log('There is a problem: ' + msg);
-    }
-}
-
-document.write('</ol>');
-
-function printTask(task) {
-    if(typeof task.name == 'undefined') {
-        throw 'The name of this task is missing! Help!';
-    } else if (typeof task.complete == 'undefined') {
-        throw 'The completion status of this task is gone. OH NO'
+    
+    var html = '<h2>My Tasks</h2>'; // contain our HTML as a string
+    
+        html +='<ol>';
+    
+    
+    for(var i= 0; i < tasks.length; i++) {
+        try {
+            html += printTask(tasks[i]);
+        } catch (msg) {
+            console.log('There is a problem: ' + msg);
+        }
     }
     
-    if(task['complete']) {
-        document.write('<li class="complete">' + task.name + '</li>');
+    html += '</ol>';
+    
+    
+    $('.container').append(html);    
+
+});
+
+function printTask(task) {
+    if(typeof task.name === 'undefined') {
+        throw 'The name of this task is missing! Help!';
+    } else if (typeof task.complete === 'undefined') {
+        throw 'The completion status of this task is gone. OH NO';
+    }
+    
+    if(task.complete) {
+        return '<li class="complete">' + task.name + '</li>';
     } else {
-        document.write('<li>' + task.name + '</li>'); 
+        return '<li>' + task.name + '</li>'; 
     }
 }
 
-
-
+function removeCompleted() {
+    $('.complete').remove();
+}
