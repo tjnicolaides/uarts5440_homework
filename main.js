9b2680a2f88624cb99120fa58f7a272f9e23fe54
TJ Nicolaides <tjnicolaides@gmail.com>
2013-07-10 22:35:28 -0400

    mostly finished object oriented example

--- main.js
+++ main.js
@@ -1,51 +1,81 @@
 
+var taskApp;
+
 $(document).ready(function(){
 
-    var tasks = [{
-        'name' : 'Do the laundry',
-        'complete' : true
-    }, {
-        'name': 'Take out the trash',
-        'complete' : false
-    }, {
-        'name' : 'Feed the dog',
-        'complete' : false
-    }];
-    
-    var html = '<h2>My Tasks</h2>'; // contain our HTML as a string
-    
-        html +='<ol>';
-    
-    
-    for(var i= 0; i < tasks.length; i++) {
-        try {
-            html += printTask(tasks[i]);
-        } catch (msg) {
-            console.log('There is a problem: ' + msg);
-        }
-    }
-    
-    html += '</ol>';
-    
-    
-    $('.container').append(html);    
+    taskApp = {
+        
+        'tasks' : {
+            'list' : [],
+            'render' : function(task) {
+                if(typeof task.name === 'undefined') {
+                    throw 'The name of this task is missing! Help!';
+                } else if (typeof task.complete === 'undefined') {
+                    throw 'The completion status of this task is gone. OH NO';
+                }
+                
+                if(task.complete) {
+                    return '<li class="complete">' + task.name + '</li>';
+                } else {
+                    if(task.urgent()) {
+                        return '<li class="urgent">' + task.name + '</li>';
 
+                    } else {
+                        return '<li>' + task.name + '</li>'; 
+                    }
+                }
+            }
+            
+        },
+        'add' : function(name, due) {
+            this.tasks.list.push(new Task(name, due));
+        },
+        'remove' : function() {
+            $('.complete').remove();
+            
+        },
+        'display' : function() {
+                var tasks = this.tasks;
+                var html = '<h2>My Tasks</h2>'; // contain our HTML as a string
+    
+                    html +='<ol>';
+
+                    for(var i= 0; i < tasks.list.length; i++) {
+                        try {
+                            html += tasks.render(tasks.list[i]);
+                        } catch (msg) {
+                            console.log('There is a problem: ' + msg);
+                        }
+                    }
+                    
+                    html += '</ol>';
+                    $('.container').html(html); 
+        },
+        'init' : function() {
+            this.add('Take out the garbage', moment('2013-07-15'));
+            this.add('Fold the laundry', moment());
+            this.add('Feed the dog');
+            this.add('Do week 7 slides');
+            this.display();
+        }
+         
+    }
+    taskApp.init();
 });
 
-function printTask(task) {
-    if(typeof task.name === 'undefined') {
-        throw 'The name of this task is missing! Help!';
-    } else if (typeof task.complete === 'undefined') {
-        throw 'The completion status of this task is gone. OH NO';
-    }
+var Task = function(name, due) {
+    // generate a task
     
-    if(task.complete) {
-        return '<li class="complete">' + task.name + '</li>';
-    } else {
-        return '<li>' + task.name + '</li>'; 
-    }
-}
+    this.name = name || 'Generic task';
+    this['complete'] = false;
+    this.due = due || moment().add('days', 10);
+    this.urgent = function() {
+        var oneWeekFromNow = moment().add('days', 7);
+        console.log(oneWeekFromNow);
 
-function removeCompleted() {
-    $('.complete').remove();
+        if(this.due.isBefore(oneWeekFromNow)) {
+            return true;
+        } 
+        return false;
+    };
 }
